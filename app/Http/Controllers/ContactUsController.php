86a31490dfb98a165915f597c67938dcced3a053
Arya <aryailham05@users.github.com>
2020-12-25 13:02:18 +0700

    Update ContactUsController.php

--- app/Http/Controllers/ContactUsController.php
+++ app/Http/Controllers/ContactUsController.php
@@ -13,7 +13,7 @@ class ContactUsController extends Controller
 
     public function __construct()
     {
-        // $this->middleware('auth');
+        $this->middleware('auth');
     }
     /**
      * Display a listing of the resource.
